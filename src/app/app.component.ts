e5605fa89ece1cb826efdbb7e901c9a7bde69a61
Yahya Boujrah <yahyaboujrah@Yahyas-MacBook-Air.local>
2023-12-01 17:00:44 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Recipe-Management-App_Frontend';
+  title = 'Recipe-Management-App';
 }
